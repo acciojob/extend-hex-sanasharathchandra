f2e30088bff848f4feb3a40bbe3d6b7740bd18f9
sanasharathchandra <93203011+sanasharathchandra@users.noreply.github.com>
2024-01-24 19:06:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const hexWithoutHash=shortHex.replace(/^#/,"");
+	if(hexWithoutHash.length!==3){
+		throw new Error("Invalid short hex code.It must have 3 characters.");
+	}
+	const chars = hexWithoutHash.split("");
+	const fullHex = "#" + chars.map((char) => char.repeat(2)).join("");
+
+  return fullHex;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+
+
